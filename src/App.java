caa531437a29f6a162ccdb933d01943d17dfdb10
Mochatitan <mochatitanstudios@gmail.com>
2025-01-19 15:36:39 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,6 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         public static String name = "johhny appleseed";
         public static int age = 17;
-        public static String luke = "Luke";
     }
 }
