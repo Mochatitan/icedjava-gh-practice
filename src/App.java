063879e6deace16437ac62545e81291b92166698
IcedJavaRobotics <57970708+IcedJavaRobotics@users.noreply.github.com>
2025-01-19 15:57:46 -0500

    Update App.java
    johnny has color

--- src/App.java
+++ src/App.java
@@ -2,6 +2,13 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         System.out.println("johnny 17");
-    }
+
+        
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
+    System.out.println(ANSI_GREEN + "johnny 17" + ANSI_RESET);
     
 }
